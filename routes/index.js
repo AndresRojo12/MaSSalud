fc2aa955ea224cc1c20986b76ff6193a55f164fc
AndresRojo12 <andresrojo20013@gmail.com>
2024-04-30 20:03:38 -0500

    se crea ruta para la peticion de login #3

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const usuariosRouter = require('./usuarios.router');
+const authRouter = require('./auth.router');
+
 
 const routerApi = (app) => {
   const router = express.Router();
   app.use('/api/v1', router)
    // se crea una ruta maestra o principal para controlar el resto de la rutas
   router.use('/usuarios', usuariosRouter);
+  router.use('/auth',authRouter);
 }
 
 module.exports = routerApi;
