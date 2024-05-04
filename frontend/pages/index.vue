068ba19cccb72ef80999aae34abd16c42a7b288f
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-03 20:50:17 -0500

    se modifica index principal de frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-toolbar color="green">
+    <template v-slot:prepend>
+      <div class="text-h5">+MasSalud</div>
+    </template>
+    <v-toolbar-items>
+      <v-btn variant="text"
+      @click="crearUsuarios">Registrarse</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script setup>
+  const router = useRouter();
+
+  const crearUsuarios = () => {
+    router.push('/registro/crear')
+  }
+</script>
