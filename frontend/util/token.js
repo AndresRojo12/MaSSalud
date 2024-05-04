919a1ec97cf6349241c118e349271bdc2ee6e216
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-03 20:55:18 -0500

    se crea logica para poder guardar token de autenticacion #4

---
+++ frontend/util/token.js
@@ -0,0 +1,11 @@
+export const storeToken = (token) => {
+  localStorage.setItem('token', token);
+};
+
+export const getToken = () => {
+  return typeof window !== 'undefined' ? localStorage.getItem('token') : null
+}
+
+export const removeToken = () => {
+  return typeof window !== 'undefined' ? localStorage.removeItem('token') : null
+}
