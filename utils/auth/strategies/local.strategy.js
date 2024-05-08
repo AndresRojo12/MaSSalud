60df5fd28ca360080fc01ed97c288b6a1d6bbd71
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-07 20:43:15 -0500

    se importa función getUser para inicio de sesión Usuarios::Login #3

--- utils/auth/strategies/local.strategy.js
+++ utils/auth/strategies/local.strategy.js
@@ -1,9 +1,8 @@
 const { Strategy } = require('passport-local');
-const boom = require('@hapi/boom');
-const bcrypt = require('bcrypt');
-const servicioUsuario = require('./../../../services/usuario.service');
 
-const servicio = new servicioUsuario();
+const AuthService = require('./../../../services/auth.service');
+
+const servicio = new AuthService();
 
 const LocalStrategy = new Strategy({
   usernameField: 'email',
@@ -11,15 +10,7 @@ const LocalStrategy = new Strategy({
 },
 async (email, password, done) => {
   try {
-    const usuario = await servicio.findByEmail(email);
-    if(!usuario){
-      done(boom.unauthorized(),false);
-    }
-    const isMatch = await bcrypt.compare(password, usuario.password);
-    if(!isMatch){
-      done(boom.unauthorized(),false);
-    }
-    delete usuario.dataValues.password;
+    const usuario = await servicio.getUser(email, password);
     done(null, usuario);
   } catch (error) {
     done(error, false);
