71fdf1eac688799fd6f1fad42820683b25c4f6d1
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-07 20:40:53 -0500

    se crean rutas para recuperación de contraseña Usuarios::Login #3

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,30 +1,51 @@
-const express = require('express');// se requiere express despues de instalarlo
+const express = require('express'); // se requiere express despues de instalarlo
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const { config } = require('../config/config');
+
+const AuthService = require('../services/auth.service');
 const router = express.Router();
+const service = new AuthService();
 
-
-router.post('/login',
-passport.authenticate('local', {session:false}),
- async (req, res, next) => {
-  try {
-
-    const user = req.user;
-    const payload = {
-      sub:user.id,
-      role:user.role
+router.post(
+  '/login',
+  passport.authenticate('local', { session: false }),
+  async (req, res, next) => {
+    try {
+      const user = req.user;
+      res.json(service.singToken(user));
+    } catch (error) {
+      next(error);
     }
-    const token = jwt.sign(payload, config.jwtSecret)
-    res.json({
-      user,
-      token
-    });
+  },
+);
 
-  } catch (error) {
-    next(error);
-  }
-});
+router.post(
+  '/recovery',
 
+  async (req, res, next) => {
+    try {
+      const { email } = req.body;
+      const rta = await service.sendRecovery(email);
+      res.json(rta);
+
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post(
+  '/change-password',
+
+  async (req, res, next) => {
+    try {
+      const { token, newPassword } = req.body;
+      const rta = await service.changePassword(token, newPassword);
+      res.json(rta);
+
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 module.exports = router;
