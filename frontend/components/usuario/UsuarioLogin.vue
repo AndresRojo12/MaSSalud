f3e64bcd590c79ebe2d761c2154b99f65e35daec
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-07 20:35:28 -0500

    se agrega función para redireccionar al cambio de contraseña Usuarios:Login #5

--- frontend/components/usuario/UsuarioLogin.vue
+++ frontend/components/usuario/UsuarioLogin.vue
@@ -2,18 +2,22 @@
   <div>
     <v-card
       class="mx-auto pa-12 pb-8"
-      style="margin-top:60px ;"
+      style="margin-top: 60px"
       elevation="8"
       max-width="380"
       rounded="lg"
     >
-      <div class="text-subtitle-1 text-medium-emphasis"
-      style="text-align: center;">Inicio de sesión</div>
+      <div
+        class="text-subtitle-1 text-medium-emphasis"
+        style="text-align: center"
+      >
+        Inicio de sesión
+      </div>
 
       <v-text-field
         v-model="email"
         density="compact"
-        style="margin-top: 20px;"
+        style="margin-top: 20px"
         placeholder="Correo electronico"
         prepend-inner-icon="mdi-email-outline"
         variant="outlined"
@@ -28,9 +32,9 @@
           class="text-caption text-decoration-none text-blue"
           href="#"
           rel="noopener noreferrer"
-          target="_blank"
+          target="_blank" @click.prevent="recuperarContrasena"
         >
-          Forgot login password?</a
+          olvido su contraseña?</a
         >
       </div>
 
@@ -45,7 +49,14 @@
         @click:append-inner="visible = !visible"
       ></v-text-field>
 
-      <v-btn class="mb-8" color="blue" size="large" variant="tonal" block @click.prevent="login">
+      <v-btn
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+        block
+        @click.prevent="login"
+      >
         Iniciar
       </v-btn>
     </v-card>
@@ -53,39 +64,44 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  import Swal from 'sweetalert2';
-  import { storeToken, getToken, removeToken } from '@/util/token.js';
+import { ref } from "vue";
+import Swal from "sweetalert2";
+import { storeToken, getToken, removeToken } from "@/util/token.js";
 
-  const CONFIG = useRuntimeConfig();
-  const router = useRouter();
-  const email = ref('');
-  const password = ref('');
-  const visible = false;
+const CONFIG = useRuntimeConfig();
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+const visible = false;
 
-  const login = async () => {
-    try {
-          const { data, error } = await useFetch(`${CONFIG.public.API_BASE_URL}auth/login`, {
-          method: 'POST',
-          body: {
-            email: email.value,
-            password: password.value
-          }
-        });
-        if (data.value !== null) {
-          storeToken(data.value.token);
-          router.push('/paciente/inicio');
-        } else {
-            Swal.fire({
-              icon: 'error',
-              title: 'Error',
-              text: error.value.data.message,
-            });
-          }
-        } catch (error) {
-          console.error("Error logging in");
-        }
-      }
+const login = async () => {
+  try {
+    const { data, error } = await useFetch(
+      `${CONFIG.public.API_BASE_URL}auth/login`,
+      {
+        method: "POST",
+        body: {
+          email: email.value,
+          password: password.value,
+        },
+      },
+    );
+    if (data.value !== null) {
+      storeToken(data.value.token);
+      router.push("/paciente/inicio");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.value.data.message,
+      });
+    }
+  } catch (error) {
+    console.error("Error logging in");
+  }
+};
 
-
+const recuperarContrasena = () => {
+  router.push('/login/recuperar_contrasena');
+}
 </script>
