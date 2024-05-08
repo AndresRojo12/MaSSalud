eeb5025b1a4e6604130aed4e1b7ea04c4725061f
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-07 20:36:35 -0500

    se crea la pagina de vista de implementación de contraseña Usuarios:Login #5

---
+++ frontend/pages/login/nueva_contrasena.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ContrasenaNueva></ContrasenaNueva>
+  </div>
+</template>
+
+<script setup>
+  import ContrasenaNueva from '~/components/usuario/ContrasenaNueva.vue';
+</script>
