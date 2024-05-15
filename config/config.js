119ebe62f69c52037dddaf575984e4d105845e11
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-15 18:12:45 -0500

    se involucran nuevas variables para usuario admin y super admin Aprovisionamiento_de_entorno::Backend #1

--- config/config.js
+++ config/config.js
@@ -9,8 +9,12 @@ const config = {
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
   jwtSecret: process.env.JWT_SECRET,
-  smtpEmail:process.env.SMTP_EMAIL,
-  smtpPassword:process.env.SMTP_PASSWORD
+  smtpEmail: process.env.SMTP_EMAIL,
+  smtpPassword: process.env.SMTP_PASSWORD,
+  emailSuperAdmin: process.env.EMAIL_SUPER_ADMIN,
+  emailAdmin: process.env.EMAIL_ADMIN,
+  AdminPassword: process.env.ADMIN_PASSWORD,
+  passwordAdmin: process.env.PASSWORD_ADMIN
 }
 
 module.exports = { config };
