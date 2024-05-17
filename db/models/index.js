a064c0a5a2c17cb89b4fe34e489ea5dd50f8bb7b
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-17 18:43:55 -0500

    se inicializa el modelo de citas medicas Crear::Citas_Medicas #7

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,13 @@
 const sequelize = require('../../libs/sequelize');
 
 const { Usuario, UsuarioSchema } = require('./usuario.model');
+const { CitaMedica, CitaSchema } = require('./citas_medicas.model');
+
 
 const setupModels = (sequelize) => {
   Usuario.init(UsuarioSchema, Usuario.config(sequelize));
+  CitaMedica.init(CitaSchema, CitaMedica.config(sequelize));
+  CitaMedica.associate(sequelize.models);
 }
 
 module.exports = setupModels;
