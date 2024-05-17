2f14761351b6f109e2879269fe9b621092f5e27c
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-17 18:52:04 -0500

    se agrega pinia en la configuración de vuetify Aprovisionamiento_entorno::Frontend #4

--- frontend/plugins/vuetify.js
+++ frontend/plugins/vuetify.js
@@ -2,13 +2,18 @@
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { createPinia } from 'pinia';
+//import { defineNuxtPlugin } from '@nuxtjs/composition-api';
+
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const pinia = createPinia();
   const vuetify = createVuetify({
     ssr: true,
     components,
     directives,
   });
 
-  nuxtApp.vueApp.use(vuetify);
+  // nuxtApp.use(pinia);
+  nuxtApp.vueApp.use(vuetify).use(pinia);
 });
