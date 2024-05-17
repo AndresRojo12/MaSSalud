a642eed93058e5b62f4e5ea69afc08f1ff6f517f
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-17 18:51:02 -0500

    se hace modificación de la interfaz principal

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,6 @@
 <template>
-  <v-toolbar color="green">
-    <template v-slot:prepend>
-      <div class="text-h5">+MasSalud</div>
-    </template>
-    <v-toolbar-items>
-      <v-btn variant="text"
-      @click="crearUsuarios">Registrarse</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+  <UsuarioLogin></UsuarioLogin>
 </template>
 
 <script setup>
-  const router = useRouter();
-
-  const crearUsuarios = () => {
-    router.push('/registro/crear')
-  }
 </script>
