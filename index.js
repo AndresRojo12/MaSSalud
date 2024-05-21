4dd48edfac7dcc0cd56eeebdab2d89fe8e7599f3
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-21 18:45:19 -0500

    produccion

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const express = require('express');
-const cors  = require('cors');
+const cors = require('cors');
 const routerApi = require('./routes');
 const path = require('path');
-
 const { logError, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
+// Middleware para parsear JSON
 app.use(express.json());
 
-// const whitelist = ['http://localhost:3001', 'https://massalud.onrender.com/'];
+// Configuración de CORS
+const whitelist = ['http://localhost:3001', 'https://massalud.onrender.com'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  credentials: true, // Permite incluir cookies y encabezados de autenticación en las solicitudes
+};
+app.use(cors(corsOptions));
 
-// const options = {
-//   origin: (origin, callback) => {
-//     if(whitelist.includes(origin)) {
-//       callback(null, true);
-//     }else {
-//       callback(new Error('no permitido'));
-//     }
-//   }
-// }
-app.use(cors());
-
+// Autenticación
 require('./utils/auth');
 
-// app.get('/', (req, res) => {
-//   res.send('server express');
-// })
-
+// Servir archivos estáticos de la aplicación frontend
 const nuxtDistPath = path.join(__dirname, './frontend/dist');
 app.use(express.static(nuxtDistPath));
 
@@ -36,12 +35,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(nuxtDistPath, 'index.html'));
 });
 
+// Configuración de rutas API
 routerApi(app);
+
+// Manejo de errores
 app.use(logError);
 app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
+// Inicio del servidor
 app.listen(port, () => {
-  console.log('corriendo por el', port);
-})
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
-
 const usuariosRouter = require('./usuarios.router');
 const citasRouter = require('./citas.router');
-
 const authRouter = require('./auth.router');
-
 
 const routerApi = (app) => {
   const router = express.Router();
-  app.use('/api/v1', router)
-   // se crea una ruta maestra o principal para controlar el resto de la rutas
+  app.use('/api/v1', router); // Ruta principal para la API
   router.use('/usuarios', usuariosRouter);
   router.use('/citas', citasRouter);
-  router.use('/auth',authRouter);
-}
+  router.use('/auth', authRouter);
+};
 
 module.exports = routerApi;
