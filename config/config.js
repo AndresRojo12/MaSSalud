3877954612bec5c9b07697b719c4e464fa3b448b
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-21 14:06:41 -0500

    produccion

--- config/config.js
+++ config/config.js
@@ -18,4 +18,26 @@ const config = {
   credentialCorreo: process.env.CREDENTIAL_CORREO
 }
 
+// config/config.js
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // solo si tu base de datos requiere SSL
+      },
+    },
+  },
+};
+
+
 module.exports = { config };
