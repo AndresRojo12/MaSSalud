cbc72a9e53983ae3dba8bc9e6e5d8e0364facc31
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-21 14:50:40 -0500

    produccion

--- db/config.js
+++ db/config.js
@@ -1,15 +1,22 @@
 const { config } = require('./../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+let URI;
+
+if (process.env.NODE_ENV === 'production') {
+  // En producción, usa la URL proporcionada por Render
+  URI = process.env.DATABASE_URL;
+} else {
+  // En desarrollo, construye la URL manualmente
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
 
 module.exports = {
   development: {
     url: URI,
     dialect: 'postgres',
   },
-
   production: {
     url: URI,
     dialect: 'postgres',
