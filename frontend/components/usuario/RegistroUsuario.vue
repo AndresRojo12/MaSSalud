9a5e043c113b1d174b5f3458d0f46a5b0b0ebbd0
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-22 12:17:57 -0500

    cambios

--- frontend/components/usuario/RegistroUsuario.vue
+++ frontend/components/usuario/RegistroUsuario.vue
@@ -216,7 +216,7 @@ const password = ref("");
 const registrarUsuario = async () => {
   //console.log('API Base URL in request:', `${CONFIG.public.API_BASE_URL}usuarios/`);
   const { data, error } = await useFetch(
-    `${CONFIG.public.API_BASE_URL}/usuarios/`,
+    `${CONFIG.public.API_BASE_URL}usuarios/`,
     {
       method: "POST",
       body: {
